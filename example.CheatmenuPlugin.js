8f88e3d8a5e01bd233c73c27237703a985fd7f5f
JustinsKittens <89991471+JustinsKittens@users.noreply.github.com>
2021-09-23 14:15:34 -0500

    Update and rename example.CrustulumPlugin.js to example.CheatmenuPlugin.js

--- example.CheatmenuPlugin.js
+++ example.CheatmenuPlugin.js
@@ -5,14 +5,14 @@
  * Load this file and then load Cheat Menu
  */
 
-if (typeof MenuPlugins !== 'object') MenuPlugins = {};
+if (typeof CheatPlugins !== 'object') CheatPlugins = {};
 
-CrMenuPlugins['moreLumps'] = { // Functions don't need defined if they aren't used -- defined here for example only
-    Init: () => { // Ran after Crustulum inits
+CheatPlugins['moreLumps'] = { // Functions don't need defined if they aren't used -- defined here for example only
+    Init: () => { // Ran after Cheat Menu inits
     },
-    Loaded: () => { // Ran after Crustulum is loaded but before Init() is called
+    Loaded: () => { // Ran after Cheat Menu is loaded but before Init() is called
     },
-    Unloaded: () => { // Ran right before Crustulum unload finishes - only ran when the user clicks the unload button
+    Unloaded: () => { // Ran right before Cheat Menu unload finishes - only ran when the user clicks the unload button
     },
     Actions: { // Only ran when we call them
         moreSugarLumps: ()=>{
@@ -21,7 +21,7 @@ CrMenuPlugins['moreLumps'] = { // Functions don't need defined if they aren't us
     },
     Game: { // Ran when Cheat Menu runs them
         UpdateMenu: (fragment) => { 
-            fragment.appendChild(Cheat.Menu.subheading('Crustulum: More Lumps Add-on'));
+            fragment.appendChild(Cheat.Menu.subheading('Cheat: More Lumps Add-on'));
             fragment.appendChild(Cheat.Menu.actionButton('giveSugarLump','Give Sugar Lump','Gives you a sugar limp.', Cheat.Plugins['moreLumps'].Actions.moreSugarLumps));
         },
     },
